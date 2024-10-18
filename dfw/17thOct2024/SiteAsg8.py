9ae6f55755b8d3bb194a90aa54f49477b66c02d2
THeZoNE-007 <ashutoshmallkvctc@gmail.com>
2024-10-18 20:54:00 +0530

    DFW - 18th Oct 2024 | Completed Asg8 | Asg9 in progress

--- dfw/17thOct2024/SiteAsg8.py
+++ dfw/17thOct2024/SiteAsg8.py
@@ -1,7 +1,7 @@
 import sys
-import subprocess as sp
-#Funtion to read nth column separated by given separator
 
+
+#Funtion to read nth column separated by given separator
 def read_nth_column(file_path, n, separator):
 	column_data = []
 	with open(file_path, 'r') as file:
@@ -13,29 +13,57 @@ def read_nth_column(file_path, n, separator):
 	return column_data
 
 
-file_path = sys.argv[1]
-n = 2
-separator = '|'
-
-result = read_nth_column(file_path, n, separator)
-print(result)
-
-"""
-# Find unique entries of given array
-
+#Function to read unique entries only
 def find_unique_entries(array):
 	return list(set(array))
 
-#Merging the two arrays
+file_path = sys.argv[1]
 
-merged_array = [f"{a}-{b}" for a, b in zip(array1, array2)]
 
-# filter string
+client = read_nth_column(file_path, 1, "|") # Array storing raw client ip/port
+cIP = [] # Array storing raw client ip only
+cPort = [] # Array storing client ports only
+for i in client:	
+	pattern = "/"
+	client = i.split(pattern)[0]
+	port = i.split(pattern)[-1] 
+	cIP.append(client)
+	cPort.append(port)
 
-s = "ab-qweqw"
-pattern = "-"
-result1 = s.split(pattern)[0]
-result2 = s[3:]
-# result1: "ab" result2: "qweqw"
-"""
+server = read_nth_column(file_path, 2, "|") # Array storing raw server ip/port
+sIP = [] # Array storing raw server ip only
+sPort = [] # Array storing server ports only
+for i in server:
+	pattern = "/"
+	client = i.split(pattern)[0]
+	port = i.split(pattern)[-1] 
+	sIP.append(client)
+	sPort.append(port)
 
+print("Q8 - (i)\n")
+print("No. of Unique Client(s) :", len(find_unique_entries(cIP)))
+print("No. of Unique Server(s) :", len(find_unique_entries(sIP)))
+
+
+print("\nQ8 - (ii)\n")
+c_s_comm = [f"{a}<->{b}" for a, b in zip(cIP, sIP)] #Client-Server merger array
+#Unique c_s_comms
+print("Unique Client-Server communications :", find_unique_entries(c_s_comm))
+
+
+print("\nQ8 - (iii)\n")
+print("Unique Client Ports :",len(find_unique_entries(cPort)))
+print("Unique Server Ports :",len(find_unique_entries(sPort)))
+
+
+print("\nQ8 - (iv & v)\n")
+recent = oldest = 0
+with open(sys.argv[1], 'r') as file:
+	l = file.readlines()
+	for i in l:
+		if i[1:20] >= l[recent][1:20]:
+			recent = l.index(i)
+		else:
+			oldest = l.index(i)
+	print(f"Recent packet :\n{l[recent]}\n")
+	print(f"Oldest packet :\n{l[oldest]}\n")
