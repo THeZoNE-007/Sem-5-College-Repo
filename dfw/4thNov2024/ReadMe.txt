5f583275b68e1d7a5ff50ad3613776f826edba0f
THeZoNE-007 <ashutoshmallkvctc@gmail.com>
2024-11-06 01:14:40 +0530

    DFW Updated - 5th Nov 2024

--- dfw/4thNov2024/ReadMe.txt
+++ dfw/4thNov2024/ReadMe.txt
@@ -1,1 +1,3 @@
-Asg 11 - not completed or submitted ofc
+Asg 11 done whole class
+
+This python file to be run in volatility3 folder along with text file containing commands in it. It automatically creates the output.log file
--- dfw/4thNov2024/asg11.py
+++ dfw/4thNov2024/asg11.py
@@ -1,28 +1,44 @@
 import subprocess as sp
 import sys
-filterCondition=False
-fileloc=sys.argv[1]
-if len(sys.argv)>=3:
-    if sys.argv[2]=="-r" and sys.argv[3]:
-        filterCondition=True
-commands=[]
-results=[]
-with open(fileloc,'r') as file:
-    commands=file.readlines()
-    file.close()
-def executeCommands(commandName):
-    res=sp.run(["sudo","python3","./volatility3/vol.py","-f","./volatility3/wcry.raw",f"{commandName}"],text=True,stdout=sp.PIPE,stderr=sp.PIPE)
-    if filterCondition:
-        output_lines=res.stdout.splitlines()
-        filtered_lines = [line for line in output_lines if sys.argv[3] in line and sys.argv[4] in line]
-        for lines in filtered_lines:
-            results.append(lines)
+
+# Initialize filter condition and file location
+is_filter_enabled = False
+input_file = sys.argv[1]
+
+# Check for filter arguments
+if len(sys.argv) >= 4 and sys.argv[2] == "-r":
+    is_filter_enabled = True
+    filter_arg1 = sys.argv[3]
+    filter_arg2 = sys.argv[4]
+
+# Read commands from the specified file
+command_list = []
+output_results = []
+
+with open(input_file, 'r') as cmd_file:
+    command_list = cmd_file.readlines()
+
+# Function to execute commands
+def run_command(command):
+    process_result = sp.run(["sudo", "python3", "./vol.py", "-f", "./wcry.raw", command], text=True, stdout=sp.PIPE, stderr=sp.PIPE)
+    
+    # Process output based on filter condition
+    if is_filter_enabled:
+        filtered_output = [
+            line for line in process_result.stdout.splitlines()
+            if filter_arg1 in line and filter_arg2 in line
+        ]
+        output_results.extend(filtered_output)
     else:
-        results.append(res.stdout)
-for cmds in commands:
-    executeCommands(cmds.split("\n")[0])
-with open("./output.log",'w+') as file:
-    for outputs in results:
-        file.writelines(f"{outputs}\n")
-    print("Log file generated in the current directory")
-    file.close()
+        output_results.append(process_result.stdout)
+
+# Execute each command from the list
+for cmd in command_list:
+    run_command(cmd.strip())
+
+# Write results to a log file
+with open("./asg11_output.log", 'w+') as log_file:
+    log_file.writelines(f"{output}\n" for output in output_results)
+
+print("Log file created !!")
+
